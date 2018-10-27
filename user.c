7378c81257aaac54b3e46ac36b096c497130cb17
Charlie Root <root@j1.D_h4h.32-bit.FWG112>
2018-10-27 04:29:33 +0000

    Changes to be committed: 	modified:   user.c

--- user.c
+++ user.c
@@ -1,16 +1,8 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-/* This program forks and and the prints whether the process is
- *   - the child (the return value of fork() is 0), or
- *   - the parent (the return value of fork() is not zero)
- *
- * When this was run 100 times on the computer the author is
- * on, only twice did the parent process execute before the
- * child process executed.
- *
- * Note, if you juxtapose two strings, the compiler automatically
- * concatenates the two, e.g., "Hello " "world!"
- */
 
 int main( void ) {
 	char *argv[3] = {"Command-line", ".", NULL};
@@ -21,8 +13,7 @@ int main( void ) {
 		execvp( "find", argv );
 	}
 
-	/* Put the parent to sleep for 2 seconds--let the child finished executing */
-	wait( 2 );
+	sleep(2);
 
 	printf( "Finished executing the parent process\n"
 	        " - the child won't get here--you will only see this once\n" );
