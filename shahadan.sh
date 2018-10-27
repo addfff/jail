a4fd41aa4b3e5f76eb42891c8cc4460ee70193bf
Charlie Root <root@j1.D_h4h.32-bit.FWG112>
2018-10-27 03:23:37 +0000

    try	modified:   shahadan.sh

--- shahadan.sh
+++ shahadan.sh
@@ -2,15 +2,16 @@
 echo 'hostname='$HOST
 hostname -s > 001-h
 H=`cat 001-h`
+DB=$'rawdb'
 STR1=$'CREATE TABLE '$H' (
 	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	firstkey VARCHAR(10) NOT NULL,
 	lastkey VARCHAR(32) NOT NULL,
 	mypoint INT(1) NOT NULL,
 	reg_date TIMESTAMP
-)'
+);'
 echo "$STR1" > 002-createtable.sql
-mysql --user=rawuser1 --password=rawuser123 rawdb < 002-createtable.sql
+mysql --user=rawuser1 --password=rawuser123 $DB < 002-createtable.sql
 cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1 > 003-randstring
 echo "First Key"
 cat  003-randstring
@@ -20,6 +21,13 @@ md5 -q 003-randstring
 L=`md5 -q 003-randstring` 
 STR2=$'INSERT INTO '$H' (firstkey, lastkey, mypoint, reg_date)
 	VALUES ("'$F'", "'$L'", 2000000000, NOW()
-)'
+);'
 echo "$STR2" > 004-insertintotable.sql
-mysql --user=rawuser1 --password=rawuser123 rawdb < 004-insertintotable.sql
+mysql --user=rawuser1 --password=rawuser123 $DB < 004-insertintotable.sql
+STR3=$'CREATE USER "'$H'"@"localhost" IDENTIFIED BY "'$F'";'
+echo "$STR3" > 005-createuser.sql
+mysql --user=root --password=toor $DB < 005-createuser.sql
+STR4=$'GRANT INSERT ON '$DB'.'$H' TO "'$H'"@"localhost";
+echo "$STR4" > 006-grantuser.sql
+mysql --user=root --password=toor < 006-grantuser.sql
+
