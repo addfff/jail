e22e272b00e2f06c549ad1f5bde04699dc36a05e
Charlie Root <root@j1.D_h4h.32-bit.FWG112>
2018-10-28 00:29:14 +0000

    user.c now execl mysql statement

--- user.c
+++ user.c
@@ -8,7 +8,8 @@ int main(int argc, char *argv[])
 {
 	int ret;
 	printf("Calling execl....\n");
-	ret = execl("/bin/sh", "sh", "user.sh", NULL);
+	/* ret = execl("/bin/sh", "sh", "user.sh", NULL);*/
+	ret = execl("/usr/local/bin/mysql", "mysql", "--user=j1", "--password=7PfA9oAwps", "j1", "<", "0013-insertintotable.sql", NULL);
 
 	printf("Failed execl.. ret = %d\n*, ret");
 
