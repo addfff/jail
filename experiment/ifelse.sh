653ce4d228e10272668f3b3df3f8237f9a4baeee
sir <nadahahs@gmail.com>
2018-10-30 16:26:38 +0800

    try ifelse

---
+++ experiment/ifelse.sh
@@ -0,0 +1,7 @@
+echo "DO YOU_LIKED_THIS_ARTICLE?"
+read YOU_LIKED_THIS_ARTICLE 
+if [ $YOU_LIKED_THIS_ARTICLE = Yes ]
+then
+  echo "Please share this page with others using the social media buttons below."
+  echo "Thank you!!!"
+fi
