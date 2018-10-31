bdf45212cbb3e46ee8938b137721600b4fe174e2
Charlie Root <root@j1.D_h4h.32-bit.FWG112>
2018-10-31 10:46:45 +0000

    semula fork

--- user-a.c
+++ user-a.c
@@ -2,60 +2,33 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-
-#define MAXCHAR 1000
+ #define MAXCHAR 1000
 int sh();
-int proga();
 int progb();
 int main(int argc, char *argv[]) 
 {
-
-	int ret;
-
-	if (fork()) {
-		if (fork()){
-			ret = progb();
-		}
-
-		else {
-			ret = proba();
-		}
+ 	int ret;
+	int pid = fork();
+ 	if ( pid ==0) {
+		ret = progb();
 	}
-	else {
-		sleep(1); 
-		ret = sh();
-	}
+	sleep(1); 
+	ret = sh();
 	return 0;
 }
-
-int sh()
+ int sh()
 {
 	int ret;
         printf("Calling execl....\n");
         ret = execl("/bin/sh", "sh", "/tmp/user.sh", NULL);
         printf("Failed execl.. ret = %d\n*, ret");
-
-        return 0;
+         return 0;
 }
-
-int proga()
-{
-        int ret;
-        printf("Calling execl....\n");
-        ret = execl("/tmp/proga", "proga", NULL);
-        printf("Failed execl.. ret = %d\n*, ret");
-
-        return 0;
-}
-
-
-int progb()
+ int progb()
 {
         int ret;
         printf("Calling execl....\n");
         ret = execl("/root/progb", "progb", NULL);
-
-        printf("Failed execl.. ret = %d\n*, ret");
-
-        return 0;
+         printf("Failed execl.. ret = %d\n*, ret");
+         return 0;
 }
--- user-b.c
+++ user-b.c
@@ -2,61 +2,35 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-
-#define MAXCHAR 1000
+ #define MAXCHAR 1000
 int proga();
-int progb();
 int sql();
 int main(int argc, char *argv[]) 
 {
 	int ret;
-
-	if(fork()) {
-		if(fork()){
-			sleep(10);
-			ret = proga();
-		}
-		else {
-			ret = progb();
-		}
+ 	if(fork()) {
+		sleep(10);
+		ret = proga();
 	}
-	else {
-		sleep(1); 
-		ret = sql();
-	}
+	sleep(1); 
+	ret = sql();
 	
 	return 0;
 }
-
-int proga()
+ int proga()
 {
 	int ret;
         printf("Calling execl....\n");
         ret = execl("/tmp/proga", "proga", NULL);
         printf("Failed execl.. ret = %d\n*, ret");
-
-        return 0;
+         return 0;
 }
-
-int progb()
-{
-        int ret;
-        printf("Calling execl....\n");
-        ret = execl("/root/progb", "progb", NULL);
-
-        printf("Failed execl.. ret = %d\n*, ret");
-
-        return 0;
-}
-
-
-int sql()
+ int sql()
 {
     FILE *fp;
     char str[MAXCHAR];
     char* filename = "/tmp/0013-insertintotable.sql";
-
-    fp = fopen(filename, "r");
+     fp = fopen(filename, "r");
     if (fp == NULL){
         printf("Could not open file %s",filename);
         return 1;
@@ -64,13 +38,9 @@ int sql()
     while (fgets(str, MAXCHAR, fp) != NULL)
         printf("%s", str);
     fclose(fp);
-
-
-        int ret;
+         int ret;
         printf("Calling execl....\n");
         ret = execl("/usr/local/bin/mysql", "mysql", "rawdb","-u", "j1","--password=7PfA9oAwps", "-e", str, NULL);
-
-        printf("Failed execl.. ret = %d\n*, ret");
-
-        return 0;
+         printf("Failed execl.. ret = %d\n*, ret");
+         return 0;
 }
